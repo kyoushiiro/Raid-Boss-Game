11eb741af65429528f4538ff67e353a5138f3604
Alfred Lam <aylam@ucsc.edu>
2020-08-25 03:37:02 -0700

    fixed initial rotation of camera

--- Source/RaidBoss/RaidBossCharacter.cpp
+++ Source/RaidBoss/RaidBossCharacter.cpp
@@ -41,7 +41,7 @@ ARaidBossCharacter::ARaidBossCharacter()
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->SetUsingAbsoluteRotation(true); // Don't want arm to rotate when character does
 	CameraBoom->TargetArmLength = 900.f;
-	CameraBoom->SetRelativeRotation(FRotator(55.f, 0.f, 0.f));
+	CameraBoom->SetRelativeRotation(FRotator(-55.f, 0.f, 0.f));
 	CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
 
 	// Create a camera...
